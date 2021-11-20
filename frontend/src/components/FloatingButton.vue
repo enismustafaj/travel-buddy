7c5f5f8c15072c893711bde1b181c5810fe1a56e
enismustafaj <40141091+enismustafaj@users.noreply.github.com>
2021-11-20 17:13:20 +0100

    feat: add new post button

---
+++ frontend/src/components/FloatingButton.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="rounded-circle position-absolute px-2" style="background-color: red">+</div>
+</template>
+
+<script>
+export default {};
+</script>
+<style></style>
--- frontend/src/views/FeedView.vue
+++ frontend/src/views/FeedView.vue
@@ -1,11 +1,55 @@
 <template>
-  <div class="justify-content-center flex-column">
-    <p>1</p>
-    <p>2</p>
+  <div>
+    <div class="d-flex justify-content-center align-items-center flex-column mt-5">
+      <div v-for="(post, index) in posts" :key="index" class="w-25">
+        <div class="card mb-3 rounded shadow-sm">
+          <div class="card-body">
+            <h5 class="card-title">{{ post.user }}</h5>
+            <p class="card-text">{{ post.body }}</p>
+            <a href="#" class="btn btn-primary">Read more</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <FloatingButton />
   </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+import FloatingButton from "../components/FloatingButton.vue";
+
+export default {
+  components: {
+    FloatingButton,
+  },
+  data() {
+    return {
+      posts: [
+        {
+          user: "user",
+          body: "body",
+        },
+        {
+          user: "user",
+          body: "body",
+        },
+        {
+          user: "user",
+          body: "body",
+        },
+        {
+          user: "user",
+          body: "body",
+        },
+      ],
+    };
+  },
+  created() {
+    axios.get("/api/posts").then((response) => {
+      this.posts = response.data;
+    });
+  },
+};
 </script>
 <style scoped></style>
