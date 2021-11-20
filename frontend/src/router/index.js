79b2146a1a9001a59078a39b3a8193c3a7c36dca
enismustafaj <40141091+enismustafaj@users.noreply.github.com>
2021-11-20 14:53:56 +0100

    feat: add new proj

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,8 @@
-import { createRouter, createWebHistory } from "vue-router";
-import SignInView from "../views/SignInView.vue";
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+
+Vue.use(VueRouter);
 
 const routes = [
   {
@@ -16,20 +19,11 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/SignInView.vue"),
   },
-
-  {
-    path: "/signup",
-    name: "SignUp",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/SignUpView.vue"),
-  },
 ];
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
   routes,
 });
 
