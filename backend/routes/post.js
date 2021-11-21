95c01febc3fcfef76bdffa0003806f67aadfbfa0
= <arnavbhaiya@gmail.com>
2021-11-21 04:27:08 +0100

    join endpoint

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const Trips = require('../models/trip');
-const Createdactivities = require('../models/createdactivities');
+const Activities = require('../models/activities');
 const router = express.Router();
 // const User = require('../models/profile')
 
@@ -12,21 +12,22 @@ router.get('/trips/:query', function (req, res, next) { //to show all trips
     });
 })
 
-router.post('/join', async (req, res, next) => {
+router.post('/activity', async (req, res, next) => {
     console.log(req.body)
-    let post = new Createdactivities({
+    let post = new Activities({
         userID: req.body.userId,
         activityID: req.body.activityId,
+        type: req.body.type
     });
     try {
         await post.save();
-        res.redirect('/'); // defines the url to be redirected to 
+        // res.redirect('/'); // defines the url to be redirected to 
     } catch (e) {
         console.log(e)
-        // res.redirect("/admin/404");//load error
     }
-    // next()
 })
+
+
 
 router.get('/trips/:query', function (req, res, next) {
     console.log(req.params)
